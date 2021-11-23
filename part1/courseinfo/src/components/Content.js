47cc21830ef8b3916379bc90e32b669f66480021
Kola <87288429+KXLAA@users.noreply.github.com>
2021-11-23 11:28:55 +0100

    Complete excercises 1.3-1.5

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,17 +1,9 @@
 import Part from "./Part";
-const Content = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}) => {
+const Content = ({ parts }) => {
+  console.log(parts);
   return (
     <>
-      <Part part1={part1} exercises1={exercises1} />
-      <Part part1={part2} exercises1={exercises2} />
-      <Part part1={part3} exercises1={exercises3} />
+      <Part parts={parts} />
     </>
   );
 };
