58695bc5a1ace98975e05ef45ae161ef6599dea2
Kola <87288429+KXLAA@users.noreply.github.com>
2021-11-23 12:47:50 +0100

    Correction for excercise 1.2

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -3,7 +3,9 @@ const Content = ({ parts }) => {
   console.log(parts);
   return (
     <>
-      <Part parts={parts} />
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
     </>
   );
 };
--- part1/courseinfo/src/components/Part.js
+++ part1/courseinfo/src/components/Part.js
@@ -1,14 +1,8 @@
-const Part = ({ parts }) => {
+const Part = ({ part, exercises }) => {
   return (
     <>
       <p>
-        {parts[0].name} {parts[0].exercises}
-      </p>
-      <p>
-        {parts[1].name} {parts[1].exercises}
-      </p>
-      <p>
-        {parts[2].name} {parts[2].exercises}
+        {part} {exercises}
       </p>
     </>
   );
