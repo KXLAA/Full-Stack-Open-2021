8db02a280050dfe708c027cada1026ee80436827
Kola <87288429+KXLAA@users.noreply.github.com>
2021-11-25 20:05:18 +0100

    Refactor exercise2.13

---
+++ part2/countries/src/components/Countries.js
@@ -0,0 +1,30 @@
+import { React, useState } from "react";
+
+export const Countries = ({ foundCountry }) => {
+  const [showMore, setShowMore] = useState(false);
+  return (
+    <>
+      <p>
+        {foundCountry.name.common}{" "}
+        <button onClick={() => setShowMore(!showMore)}>show</button>
+      </p>
+
+      {showMore && (
+        <div>
+          <h1>{foundCountry.name.common}</h1>
+          <p>{foundCountry?.capital?.map((sing) => sing)}</p>
+          <p>{foundCountry.population}</p>
+
+          <h3>Languages</h3>
+          <ul>
+            {/* get values from the foundCountry.languages object & convert those values into an array & use map to get individual values */}
+            {Object.values(foundCountry.languages).map((language) => (
+              <li>{language}</li>
+            ))}
+          </ul>
+          <img alt={foundCountry.name.common} src={foundCountry.flags.svg} />
+        </div>
+      )}
+    </>
+  );
+};
--- part2/countries/src/components/Results.js
+++ part2/countries/src/components/Results.js
@@ -1,39 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
+import { Countries } from "./Countries";
 
 export const Results = ({ foundCountries }) => {
-  const [showMore, setShowMore] = useState(false);
-
-  const handleShow = (area) => {
-    setShowMore(area);
-  };
-
   if (foundCountries.length > 10) {
     return <p>Too many matches, specify another filter</p>;
   }
+
   if (foundCountries.length < 10 && foundCountries.length > 1) {
     return foundCountries.map((foundCountry) => (
       <>
-        <p key={foundCountry.area}>
-          {foundCountry.name.common}{" "}
-          <button onClick={() => handleShow(foundCountry.area)}>show</button>
-        </p>
-        {/* If showMore === foundCountry.area, show details on click */}
-        {showMore === foundCountry.area && (
-          <div>
-            <h1>{foundCountry.name.common}</h1>
-            <p>{foundCountry?.capital.map((sing) => sing)}</p>
-            <p>{foundCountry.population}</p>
-
-            <h3>Languages</h3>
-            <ul>
-              {/* get values from the foundCountry.languages object & convert those values into an array & use map to get individual values */}
-              {Object.values(foundCountry.languages).map((language) => (
-                <li>{language}</li>
-              ))}
-            </ul>
-            <img alt={foundCountry.name.common} src={foundCountry.flags.svg} />
-          </div>
-        )}
+        <Countries key={foundCountry.area} foundCountry={foundCountry} />
       </>
     ));
   }
